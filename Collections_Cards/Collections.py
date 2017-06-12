f55df476d550cec3370f1e94e120a35132634cb5
“TonyzBi” <hai_bi@163.com>
2017-06-12 19:14:33 +0800

    Vector

--- Collections_Cards/Collections.py
+++ Collections_Cards/Collections.py
@@ -3,6 +3,10 @@
 
 import collections
 
+import random
+
+from collections import OrderedDict
+
 Card = collections.namedtuple('Card',['rank', 'suit'])
 
 class FrenchDesk():
@@ -19,15 +23,40 @@ class FrenchDesk():
     def __getitem__(self, position):
         return self._cards[position]
 
+suit_values=dict(spades=3, diamonds=2, clubs=1, hearts=0)
+print suit_values
+
+def com_values(card):
+    rank_value = FrenchDesk.ranks.index(card.rank)
+    return rank_value * len(suit_values) + suit_values[card.suit]
+
+desk = FrenchDesk()
+
+for card in sorted(desk, key=com_values):
+    print card
+
+
 
 beer_card = Card('7','diamonds')
 
 print beer_card
 
-desk = FrenchDesk()
+
 
 print len(desk)
 
-for card in desk:
-    print card
+#for card in desk:
+#    print card
+
+for x in range(1, 11):
+    print random.choice(desk)
+
+items = (('A', 1), ('B', 2), ('C', 3))
+rDict = dict(items)
+oDict = OrderedDict(items)
+
+print rDict
+for k, v in oDict.items():
+    print k, v
+
 
---
+++ Collections_Cards/Vector.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+
+from math import hypot
+
+class Vector:
+
+    def __init__(self, x=0, y=0):
+        self.x = x
+        self.y = y
+
+    def __repr__(self):
+        return "Vector(%r, %r)" % (self.x, self.y)
+
+    def __abs__(self):
+        return hypot(self.x, self.y)
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        return Vector(self.x * other, self.y * other)
+
+v1 = Vector(2,3)
+v2 = Vector(4,6)
+
+print v1 + v2
+print v1 * 6
+print abs(v1)
+
+
